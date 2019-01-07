b56388af8a1854d655c11bfc6de1ea79e7cdf4df
Jim Owens <jimcowens@gmail.com>
2019-01-07 12:08:17 -0500

    pushing to get everything uploaded

--- jim_map_test/main.js
+++ jim_map_test/main.js
@@ -1,9 +1,11 @@
 let toggle =  `visible` // set variable to allow button to toggle map appearance
+
 function initMap() {
     var gaMiddle = {lat: 32.838131, lng: -83.634705}
     var map = new google.maps.Map(document.getElementById('map'), {zoom: 7, center: gaMiddle})
     }
-$('.button').click((event) => {
+
+    $('.button').click((event) => {
     event.preventDefault()
     if (toggle === `visible`){
         $('#map').css(`visibility`, toggle)
