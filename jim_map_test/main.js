9fb8814ec5df49b0ba969c961fb2345df81c9a8c
Jim Owens <jimcowens@gmail.com>
2019-01-08 11:02:16 -0500

    modified to make functional for main project

--- jim_map_test/main.js
+++ jim_map_test/main.js
@@ -1,11 +1,69 @@
-let toggle =  `visible` // set variable to allow button to toggle map appearance
+let toggle = `hidden`
 
-function initMap() {
-    var gaMiddle = {lat: 32.838131, lng: -83.634705}
-    var map = new google.maps.Map(document.getElementById('map'), {zoom: 7, center: gaMiddle})
-    }
+let farmInfo = [
+    {   
+        farmName: "Aluma Farm",
+        blurb: "",       
+    },
 
-    $('.button').click((event) => {
+    {
+        farmName: "Tucker Farms Georgia",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Truly Living Well",
+        blurb: "",       
+    },
+
+    {
+        farmName: "DogHead Farm",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Country Gardens and Farm Nursery",
+        blurb: "",       
+    },
+
+    {
+        farmName: "HunterCattle Company",
+        blurb: "",       
+    },
+
+    {
+        farmName: "White Oak Pastures",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Heritage Farm Georgia",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Mountain Valley Farm",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Love is Love Farm at Gaia Gardens",
+        blurb: "",       
+    },
+
+    {
+        farmName: "Rise 'n Shine Organic Farm",
+        blurb: "",       
+    },
+
+    {
+        farmName: "HoneyWood Farms",
+        blurb: "",       
+    },
+
+]
+
+$('.button').click((event) => {
     event.preventDefault()
     if (toggle === `visible`){
         $('#map').css(`visibility`, toggle)
@@ -15,3 +73,30 @@ function initMap() {
         toggle = `visible`
     }  
 })
+
+farmInfo.forEach(element => {
+    let mapURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${element.farmName}&key=${apiKey}`
+    console.log(element)
+    $.getJSON(mapURL, (info)=>{
+        let address = info.results[0].formatted_address
+        let latLong = info.results[0].geometry.location
+        element.address = address
+        element.latLong = latLong
+        
+    })
+
+})
+function initMap() {
+    var gaMiddle = {lat: 32.838131, lng: -83.634705}
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 7, center: gaMiddle})
+    map.setMapTypeId(`hybrid`)
+    farmInfo.forEach(element => {
+        let marker = new google.maps.Marker({
+            position: element.latLong,
+            map:map,
+            icon: `./images/map_icon.png`,
+            animation: google.maps.Animation.BOUNCE
+        })
+        marker.setMap(map)
+    })
+}
